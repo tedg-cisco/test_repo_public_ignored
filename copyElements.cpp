e251ab3c77cd16e30ec9cf9de7c957082453ab5a
Ted Gauthier <tedg-cisco@users.noreply.github.com>
2017-01-17 10:50:22 -0800

    Create copyElements.cpp
    initial commit for testing

---
+++ copyElements.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+#include <iterator>
+std::vector<int> target2(5);
+std::vector<int> target3;
+template <typename RangeOfInts>
+void foo(RangeOfInts source)
+{
+	std::vector<int> target1{std::begin(source),
+	                         std::end(source)};
+	std::copy(std::begin(source), std::end(source),
+	          std::begin(target2));
+	std::copy(std::begin(source), std::end(source),
+	          std::back_inserter(target3));
+}
